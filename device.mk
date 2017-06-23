57707cf4a864de52e425ec7fa34fec389374c5e3
TheStrix <parthbhatia98@gmail.com>
2017-06-23 14:31:52 +0530

    sagit: Add fs_config support
    Change-Id: Ica5f8fff2f120d2214389871b420b68208ba3221

--- device.mk
+++ device.mk
@@ -126,6 +126,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/calib.cfg:system/etc/calib.cfg
 
+# For android_filesystem_config.h
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # IPA Manager
 PRODUCT_PACKAGES += \
     ipacm \
