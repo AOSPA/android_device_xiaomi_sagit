dec0cf1f170163cbf1e6c14babe64c5d5a958bb1
TheStrix <parthbhatia98@gmail.com>
2017-09-23 00:22:57 +0530

    sagit: Add msm irq balance config
    Change-Id: I491fd7ca90dcbcf35f8b9b5288529052eb33f8dd

--- device.mk
+++ device.mk
@@ -219,6 +219,10 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
+# IRQ balance
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8998
