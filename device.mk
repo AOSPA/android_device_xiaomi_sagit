6e3377c26d8e66effef06d349f0f6ab631f5a191
TheStrix <parthbhatia98@gmail.com>
2018-01-04 19:56:07 +0530

    sagit: Build ANT+ packages
    Change-Id: Idd8b5cda10c7b8f92f1768b7d6aec32b548f076d

--- device.mk
+++ device.mk
@@ -81,6 +81,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    antradio_app \
+    libantradio
+
 # Audio
 -include $(TOPDIR)hardware/qcom/audio/configs/msm8998/msm8998.mk
 
