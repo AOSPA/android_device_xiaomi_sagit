d80e217135773016bd54ddb6a4c53a15c571d6ad
TheStrix <parthbhatia98@gmail.com>
2018-07-13 19:52:30 +0530

    sagit: Add vendor public libraries list
    Change-Id: Ife17ca758fe29172261869db2b02cc72d93174d8

--- device.mk
+++ device.mk
@@ -331,6 +331,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
+
 # QPerformance
 PRODUCT_BOOT_JARS += \
     QPerformance
