a165531277371cac5a4171db0ea19382c1b56e4c
TheStrix <parthbhatia98@gmail.com>
2018-09-27 18:34:00 +0530

    sagit: Update thermal HAL to compile with VNDK
    Change-Id: I58a71c5e7c9528dae527cfbb0ccb84a70ee88eb6

--- thermal/Android.mk
+++ thermal/Android.mk
@@ -36,6 +36,7 @@ ifeq ($(SUPPORT_THERMAL_HAL),)
 LOCAL_SRC_FILES := thermal-default.c
 endif
 
+LOCAL_HEADER_LIBRARIES := libutils_headers libhardware_headers
 LOCAL_SHARED_LIBRARIES := liblog libcutils
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS := -Wno-unused-parameter
